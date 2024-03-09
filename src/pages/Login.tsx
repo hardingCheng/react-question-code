077161081962087a42820208928897a96c2c0df2
hardingcheng <hardingcheng@163.com>
2024-03-09 23:02:58 +0800

    🚀 新增: 注册登录基本样式和逻辑

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,88 @@
 import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Button, Checkbox, Form, Input, Space, Typography } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Login.module.scss'
+import { Link } from 'react-router-dom'
+import { REGISTER_PATHNAME } from '../router'
+const { Title } = Typography
+const onFinish = (values: never) => {
+  console.log('Success:', values)
+}
 const Login: FC = () => {
-  const nav = useNavigate()
   return (
-    <>
-      <div>Login</div>
-      <button onClick={() => nav(-1)}>返回</button>
-    </>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 6,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 6,
+              span: 16,
+            }}
+            name="remember"
+            valuePropName="checked"
+          >
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 6,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+            <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 export default Login
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,102 @@
 import React, { FC } from 'react'
+import { Button, Form, Input, Space, Typography } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+import { LOGIN_PATHNAME } from '../router'
+const { Title } = Typography
+const onFinish = (values: never) => {
+  console.log('Success:', values)
+}
 const Register: FC = () => {
   return (
-    <>
-      <div>Register</div>
-    </>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 6,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="确认密码"
+            name="confirm"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="昵称"
+            name="nickname"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 6,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              注册
+            </Button>
+            <Link to={LOGIN_PATHNAME}>已有账户，登录！</Link>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 export default Register
